fdd80d7cba0e953ba98a8cc28c23bb5b873ea42e
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-04-27 22:19:04 +0800

    adding function in progress

---
+++ src/script.js
@@ -0,0 +1,97 @@
+let compResult = 0;
+let inputVal;
+
+const displayBar = document.getElementById("display-el");
+
+const btnZero = document.getElementById("btn0-el");
+const btnOne = document.getElementById("btn1-el");
+const btnTwo = document.getElementById("btn2-el");
+const btnThree = document.getElementById("btn3-el");
+const btnFour = document.getElementById("btn4-el");
+const btnFive = document.getElementById("btn5-el");
+const btnSix = document.getElementById("btn6-el");
+const btnSeven = document.getElementById("btn7-el");
+const btnEight = document.getElementById("btn8-el");
+const btnNine = document.getElementById("btn9-el");
+
+const btnAdd = document.getElementById("btnadd-el");
+const btnSubtract = document.getElementById("btnsub-el");
+const btnMultiply = document.getElementById("btnmulti-el");
+const btnDivide = document.getElementById("btndiv-el");
+
+const btnDecimal = document.getElementById("btndec-el");
+const btnEqual = document.getElementById("btnequal-el");
+
+function sum(){
+    
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide() {
+    
+}
+
+function equal() {
+    
+}
+
+function decimal() {
+    displayBar.innerText += ".";
+}
+
+function zeroBtn() {
+    inputVal = "0";
+    displayBar.innerText += inputVal;
+}
+
+function oneBtn() {
+    inputVal = "1";
+    displayBar.innerText += inputVal;
+}
+
+function twoBtn() {
+    inputVal = "2";
+    displayBar.innerText += inputVal;
+}
+
+function threeBtn() {
+    inputVal = "3";
+    displayBar.innerText += inputVal;
+}
+
+function fourBtn() {
+    inputVal = "4";
+    displayBar.innerText += inputVal;
+}
+
+function fiveBtn() {
+    inputVal = "5";
+    displayBar.innerText += inputVal;
+}
+
+function sixBtn() {
+    inputVal = "6";
+    displayBar.innerText += inputVal;
+}
+
+function sevenBtn() {
+    inputVal = "7";
+    displayBar.innerText += inputVal;
+}
+
+function eightBtn() {
+    inputVal = "8";
+    displayBar.innerText += inputVal;
+}
+
+function nineBtn() {
+    inputVal = "9";
+    displayBar.innerText += inputVal;
+}
